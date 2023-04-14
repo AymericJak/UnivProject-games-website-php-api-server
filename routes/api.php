24ec530f9ad7c228fc674c4d223491f481c45f98
Maxence Dewadder <maxence_dewadder@ens.univ-artois.fr>
2023-04-14 16:08:55 +0200

    Jeu

--- routes/api.php
+++ routes/api.php
@@ -35,10 +35,14 @@ Route::get('jeu/FiltrageJoueursMin', [\App\Http\Controllers\Api\JeuController::c
 Route::get('jeu/FiltrageJoueursMax', [\App\Http\Controllers\Api\JeuController::class, 'indexFiltrageJoueursMax']);
 Route::get('jeu/FiltrageMostLiked', [\App\Http\Controllers\Api\JeuController::class, 'indexMostLiked']);
 Route::get('jeu/FiltrageBestRated', [\App\Http\Controllers\Api\JeuController::class, 'indexBestRated']);
+
 Route::post('jeu', [\App\Http\Controllers\Api\JeuController::class, 'store']);
-Route::patch('jeu/{id}', [\App\Http\Controllers\Api\JeuController::class, 'edit']);
+Route::patch('jeu/{id}/edit', [\App\Http\Controllers\Api\JeuController::class, 'edit']);
+Route::patch('jeu/{id}/editUrl', [\App\Http\Controllers\Api\JeuController::class, 'edit_url']);
+
 Route::post('jeu/{id}/achat', [\App\Http\Controllers\Api\JeuController::class, 'achat']);
 Route::delete('jeu/{id}', [\App\Http\Controllers\Api\JeuController::class, 'destroy']);
+Route::get('jeu/{id}', [\App\Http\Controllers\Api\JeuController::class, 'show']);
 
 
 Route::get('editeurs', [EditeurController::class, 'index']);
