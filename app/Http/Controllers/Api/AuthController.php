47f456213cbd89c8334924a7c9a2c2519699cb04
AymericJak <aymericjakpro@gmail.com>
2023-04-14 21:51:59 +0200

    Début documentation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,6 +9,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use OpenApi\Attributes as OA;
+
 
 class AuthController extends Controller {
 
@@ -16,6 +18,52 @@ class AuthController extends Controller {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
+    #[OA\Schema(
+        schema: "UserResource",
+        properties: [
+            new OA\Property(property: "id", description: "identifiant de l'adhérent", type: "integer", format: "int54"),
+            new OA\Property(property: "login", description: "login de l'adhérent", type: "string"),
+            new OA\Property(property: "email", description: "email de l'adhérent", type: "string"),
+            new OA\Property(property: "valide", description: "Statut de l'adhérent", type: "boolean"),
+            new OA\Property(property: "nom", description: "nom de l'adhérent", type: "string"),
+            new OA\Property(property: "prenom", description: "prenom de l'adhérent", type: "string"),
+            new OA\Property(property: "pseudo", description: "pseudo de l'adhérent", type: "string"),
+            new OA\Property(property: "avatar", description: "avatar de l'adhérent", type: "string"),
+
+        ]
+    )]
+    #[OA\Post(
+        path: "/api/login",
+        operationId: "login",
+        description: "Permet à un adhérent de se connecter",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(properties: [
+                new OA\Property(property: "email", description: "Email de l'adhérent", type: "string", format: "email"),
+                new OA\Property(property: "password", description: "Mot de passe de l'adhérent", type: "string", format: "password"),
+            ]),
+        ),
+        tags: ["Utilisateur"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Adhérent connecté",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                    new OA\Property(property: "adherent", ref: "#/components/schemas/UserResource")
+                ], type: "object")
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Erreur - Unauthorized",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                ], type: "object")
+            ),
+        ]
+    )]
     public function login(Request $request) {
         $request->validate([
             'email' => 'required|string|email',
@@ -42,6 +90,52 @@ class AuthController extends Controller {
         ]);
     }
 
+    #[OA\Post(
+        path: "/api/register",
+        operationId: "register",
+        description: "Permet à un utilisateur quelconque de créer un compte",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(properties: [
+                new OA\Property(property: "login", description: "login de l'adhérent", type: "string", format: "login"),
+                new OA\Property(property: "email", description: "Email de l'adhérent", type: "string", format: "email"),
+                new OA\Property(property: "password", description: "Mot de passe de l'adhérent", type: "string", format: "password"),
+                new OA\Property(property: "nom", description: "Nom de l'adhérent", type: "string", format: "nom"),
+                new OA\Property(property: "prenom", description: "Prénom de l'adhérent", type: "string", format: "prenom"),
+                new OA\Property(property: "pseudo", description: "Pseudo de l'adhérent", type: "string", format: "pseudo"),
+            ]),
+        ),
+        tags: ["Utilisateur"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Adhérent connecté",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                    new OA\Property(property: "adherent", ref: "#/components/schemas/UserResource"),
+                    new OA\Property(
+                        property: "authorisation",
+                        properties: [
+                            new OA\Property(property: "token", type: "string"),
+                            new OA\Property(property: "type", type: "string"),
+                        ],
+                        type: "object"
+                    ),
+                ], type: "object")
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Erreur - Unauthorized",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "errors", properties: [
+                        new OA\Property(property: "id", type: "array", items: new OA\Items(type: "string"))
+                    ], type: "object"),
+                ], type: "object")
+            ),
+        ]
+    )]
     public function register(Request $request) {
         $request->validate([
             'login' => 'required|string|max:255|unique:users',
@@ -83,6 +177,29 @@ class AuthController extends Controller {
         ]);
     }
 
+    #[OA\Post(
+        path: "/api/logout",
+        operationId: "logout",
+        description: "Permet à un adhérent de se déconnecter",
+        tags: ["Utilisateur"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Adhérent déconnecté",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                ], type: "object")
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Unauthorized",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "message", type: "string"),
+                ], type: "object")
+            ),
+        ]
+    )]
     public function logout() {
         Auth::logout();
         return response()->json([
@@ -102,6 +219,42 @@ class AuthController extends Controller {
         ]);
     }
 
+    #[OA\Get( //TODO Paramètre id_user
+        path: "/api/login",
+        operationId: "login",
+        description: "Permet à un adhérent de se connecter",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(properties: [
+                new OA\Property(property: "email", description: "Email de l'adhérent", type: "string", format: "email"),
+                new OA\Property(property: "password", description: "Mot de passe de l'adhérent", type: "string", format: "password"),
+            ]),
+        ),
+        tags: ["Utilisateur"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Adhérent connecté",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                    new OA\Property(property: "adherent", ref: "#/components/schemas/UserResource"),
+                    new OA\Property(property: "commentaires", type: "string"), //TODO
+                    new OA\Property(property: "achats", type: "string"), //TODO
+                    new OA\Property(property: "likes", type: "string"), //TODO
+
+                ], type: "object")
+            ),
+            new OA\Response(
+                response: 403,
+                description: "Erreur - Unauthorized",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                ], type: "object")
+            ),
+        ]
+    )]
     public function profil($user_id) {
         $user = User::findOrFail($user_id);
         if (!Auth::check() || (Auth::user()->id != $user_id && !Auth::user()->isAdmin())) {
@@ -121,6 +274,38 @@ class AuthController extends Controller {
         ]);
     }
 
+    #[OA\Put(
+        path: "/api/login",
+        operationId: "login",
+        description: "Permet à un adhérent de se connecter",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(properties: [
+                new OA\Property(property: "email", description: "Email de l'adhérent", type: "string", format: "email"),
+                new OA\Property(property: "password", description: "Mot de passe de l'adhérent", type: "string", format: "password"),
+            ]),
+        ),
+        tags: ["Utilisateur"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Adhérent connecté",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                    new OA\Property(property: "adherent", ref: "#/components/schemas/UserResource")
+                ], type: "object")
+            ),
+            new OA\Response(
+                response: 401,
+                description: "Erreur - Unauthorized",
+                content: new OA\JsonContent(properties: [
+                    new OA\Property(property: "status", type: "string"),
+                    new OA\Property(property: "message", type: "string"),
+                ], type: "object")
+            ),
+        ]
+    )]
     public function update(AdherentRequest $request, $user_id) {
         if (!Auth::user()->isAdmin() && Auth::user()->id != $user_id) {
             return response()->json([
