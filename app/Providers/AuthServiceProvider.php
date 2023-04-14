049074271f3bd152c7b32e18bdfb8660a05f46cd
Thomas Sontag <thomas_sontag@ens.univ-artois.fr>
2023-04-14 16:04:37 +0200

    gestion des permissions close #30

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+        Gate::define('delete-commentaire', function ($user, $commentaire) {
+            return $user->id === $commentaire->user_id || $user->roles()->pluck('nom')->contains('administrateur') || $user->roles()->pluck('nom')->contains('commentaire-moderateur');
+        });
+        Gate::define('update-commentaire', function ($user, $commentaire) {
+            return $user->id === $commentaire->user_id || $user->roles()->pluck('nom')->contains('administrateur') || $user->roles()->pluck('nom')->contains('commentaire-moderateur');
+        });
+        Gate::define('store-commentaire', function ($user) {
+            return $user->roles()->pluck('id')->contains(1)||
+                $user->roles()->pluck('id')->contains(2) ||
+                $user->roles()->pluck('id')->contains(3) ||
+                $user->roles()->pluck('id')->contains(4) ||
+                $user->roles()->pluck('id')->contains(5);
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -36,3 +36,7 @@ Route::get('jeu/FiltrageMostLiked', [\App\Http\Controllers\Api\JeuController::cl
 Route::get('jeu/FiltrageBestRated', [\App\Http\Controllers\Api\JeuController::class, 'indexBestRated']);
 
 Route::post('jeu', [\App\Http\Controllers\Api\JeuController::class, 'store']);
+
+Route::post('commentaire', [\App\Http\Controllers\Api\CommentaireController::class,'store'])->middleware(['auth', 'role:adherent','role:visiteur','role:adherent-premium','role:commentaire-moderateur','role:administrateur']);
+Route::delete('/commentaires/{id}', [\App\Http\Controllers\Api\CommentaireController::class,'destroy'])->middleware(['auth','role:administrateur','role:commentaire-moderateur']);
+Route::patch('/commentaires/{id}', [\App\Http\Controllers\Api\CommentaireController::class,'update'])->middleware(['auth','role:administrateur','role:commentaire-moderateur']);
